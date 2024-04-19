8aeb494a0f9aac836eb504f5e49b5e54260aa8ce
Paula Zaidel <paulazaaidel@gmail.com>
2024-04-19 17:03:36 -0300

    create patient app

--- healing/urls.py
+++ healing/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("users/", include("users.urls")),
     path("doctors/", include("doctors.urls")),
+    path("patients/", include("patients.urls")),
 ]
